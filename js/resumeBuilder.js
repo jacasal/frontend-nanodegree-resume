30e3b8c8a2cc88077f87d288efb44bd0b7742fef
jacasal <joseandrescasalnantes@MacBook-Air-de-Jose.local>
2015-05-25 16:32:31 +0200

    ñadimos variables.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,9 @@
 $("#main").append("Jose Andres");
+
+var firstName = "Jose Andres"
+var age =43;
+var awesomeThoughts = "I am Jose Andres and I am AWESOME!!"
+console.log(firstName);
+console.log(age);
+console.log (awesomeThoughts);
+
