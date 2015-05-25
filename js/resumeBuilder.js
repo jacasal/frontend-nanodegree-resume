c2184082550aa59246853d7d7a0cff4091e6087b
jacasal <joseandrescasalnantes@MacBook-Air-de-Jose.local>
2015-05-25 23:27:42 +0200

    añado la funcion [string].replace([old],[new])

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,17 @@
-$("#main").append("Jose Andres");
+//$("#main").append("Jose Andres");
 
-var firstName = "Jose Andres"
-var age =43;
+
+//var firstName = "Jose Andres"
+//var age =43;
 var awesomeThoughts = "I am Jose Andres and I am AWESOME!!"
-console.log(firstName);
-console.log(age);
+//console.log(firstName);
+//console.log(age);
+
+var funThoughts = 
+    awesomeThoughts.replace("AWESOME","FUN");
+
+$("#main").append(funThoughts);
+
+
 console.log (awesomeThoughts);
 
