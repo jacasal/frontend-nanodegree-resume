cdade1eee3a7763dc009956b68e881eb20fd2ca6
jacasal <joseandrescasalnantes@MacBook-Air-de-Jose.local>
2015-05-28 16:15:21 +0200

    cambios de la leccion uno.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,15 +3,23 @@
 
 //var firstName = "Jose Andres"
 //var age =43;
-var awesomeThoughts = "I am Jose Andres and I am AWESOME!!"
+//var awesomeThoughts = "I am Jose Andres and I am AWESOME!!"
 //console.log(firstName);
 //console.log(age);
 
-var funThoughts = 
-    awesomeThoughts.replace("AWESOME","FUN");
+//var funThoughts = 
+//    awesomeThoughts.replace("AWESOME","FUN");
 
-$("#main").append(funThoughts);
+//$("#main").append(funThoughts);
 
 
-console.log (awesomeThoughts);
+//console.log (awesomeThoughts);
+
+
+// Lesson 1 - Data types > Mixing.replace() and .append() ( como reeplazar una cadena por otra y añadirla en el html).
+
+var NombreConformato = HTMLheaderName.replace("%data%", "Jose Andres C. N.");
+$("#header").prepend(NombreConformato);
+var PuestoConformato = HTMLheaderRole.replace("%data%", "Telecomunicaciones y Web");
+$("#header").append(PuestoConformato);  
 
